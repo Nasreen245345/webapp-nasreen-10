3a14f4b5d5ad3f6788505412d864bf751ea79476
Nasreen245345 <fnasreen950@gmail.com>
2026-04-23 14:38:12 +0500

    Initial commit

--- Cloud_Mid/src/App.jsx
+++ Cloud_Mid/src/App.jsx
@@ -5,13 +5,13 @@ function App() {
     <>
       <section id="center">
         <div>
-          <h1>Welcome Faisal</h1>
-          <h2>Name: Nasreen Fatima</h2>
-          <h3>Roll No: 12345</h3>
+          <h1>Welcome</h1>
+          <h2 style={{ color: 'black' }}>Name: Nasreen Fatima</h2>
+          <h3 style={{ color: 'black' }}>Registrtion Number: 10</h3>
         </div>
       </section>
     </>
-  )
+  ) 
 }
 
 export default App
