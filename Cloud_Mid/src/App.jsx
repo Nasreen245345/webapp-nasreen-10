fbb85992242562b31a0c4d76e7358b3fe6c3ab76
Nasreen245345 <fnasreen950@gmail.com>
2026-04-23 10:45:12 +0500

    Add prject

---
+++ Cloud_Mid/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Welcome</h1>
+          <h2>Name: Nasreen Fatima</h2>
+          <h3>Roll No: 12345</h3>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
