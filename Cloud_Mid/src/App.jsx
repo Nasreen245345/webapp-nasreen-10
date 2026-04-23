5515a75bb33d03bc6b469e1d3d7145d643cf2fda
Nasreen245345 <fnasreen950@gmail.com>
2026-04-23 12:04:38 +0500

    change code

--- Cloud_Mid/src/App.jsx
+++ Cloud_Mid/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <section id="center">
         <div>
-          <h1>Welcome</h1>
+          <h1>Welcome Faisal</h1>
           <h2>Name: Nasreen Fatima</h2>
           <h3>Roll No: 12345</h3>
         </div>
